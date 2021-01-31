e1027338d2ae235d11bd9b9a73e6c8829e521bbd
Rob O'Gara <rgo594@gmail.com>
2021-01-30 20:51:53 -0500

    first commit

---
+++ Assets/Scripts/TacticsMove.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsMove : MonoBehaviour
+{
+    public bool turn = false;
+
+    List<Tile> selectableTiles = new List<Tile>();
+    GameObject[] tiles;
+
+    Stack<Tile> path = new Stack<Tile>();
+    Tile currentTile;
+
+    public bool moving = false;
+    public int move = 5;
+    public float jumpHeight = 2;
+    public float moveSpeed = 2;
+
+    float halfHeight = 0;
+
+
+    public Tile actualTargetTile;
+
+    protected void Init()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        halfHeight = GetComponent<Collider>().bounds.extents.y;
+
+        //TurnManager.AddUnit(this);
+    }
+
+    public void GetCurrentTile()
+    {
+        currentTile = GetTargetTile(gameObject);
+        currentTile.current = true;
+    }
+
+    public Tile GetTargetTile(GameObject target)
+    {
+        RaycastHit hit;
+        Tile tile = null;
+
+        if (Physics.Raycast(target.transform.position, -Vector3.up, out hit, 1))
+        {
+            tile = hit.collider.GetComponent<Tile>();
+        }
+
+        return tile;
+    }
+
+    public void ComputeAdjacencyLists(float jumpHeight, Tile target)
+    {
+        //tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (GameObject tile in tiles)
+        {
+            Tile t = tile.GetComponent<Tile>();
+            t.FindNeighbors(jumpHeight);
+        }
+    }
+
+    public void FindSelectableTiles()
+    {
+        ComputeAdjacencyLists(jumpHeight, null);
+        GetCurrentTile();
+
+        Queue<Tile> process = new Queue<Tile>();
+
+        process.Enqueue(currentTile);
+        currentTile.visited = true;
+        //currentTile.parent = ??  leave as null 
+
+        while (process.Count > 0)
+        {
+            Tile t = process.Dequeue();
+
+            selectableTiles.Add(t);
+            t.selectable = true;
+
+            if (t.distance < move)
+            {
+                foreach (Tile tile in t.adjacencyList)
+                {
+                    if (!tile.visited)
+                    {
+                        tile.parent = t;
+                        tile.visited = true;
+                        tile.distance = 1 + t.distance;
+                        process.Enqueue(tile);
+                    }
+                }
+            }
+        }
+    }
+
+    public void MoveToTile(Tile tile)
+    {
+        path.Clear();
+        tile.target = true;
+        moving = true;
+
+        Tile next = tile;
+        while (next != null)
+        {
+            path.Push(next);
+            next = next.parent;
+        }
+    }
+}
